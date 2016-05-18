6f6449f736984b38e16dad0bf8bb950ed8b0a755
Chad Latham <chadlatham33@gmail.com>
2016-05-18 14:11:41 -0700

    Update week3

---
+++ week3/warmups.js
@@ -0,0 +1,9 @@
+// Morning Warmup 5/18/16 - Sum all inclusive numbers between two inputs.
+var sumRange = function (num1, num2) {
+
+}
+
+
+console.log(sumRange(10,20));
+// Interesting solution involves an algorithm where the sum of all numbers from 0 to a positive number is equal to the equation (n * (n + 1)) / 2
+// Figure out the correct correct code.
